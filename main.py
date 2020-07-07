8824336b11b69cec351fe861a8afb542cb7a162c
Daniel <mike>
2020-07-07 20:19:54 +0300

    second commit

--- main.py
+++ main.py
@@ -1,1 +1,41 @@
-"" 
+from flask import Flask
+import threading
+import time
+
+def intense_cpu():
+    def run():
+        while True:
+            print('thread running')
+            global stop_threads
+            if stop_threads:
+                break
+
+    threads = []
+
+    for i in range(10):
+        t1 = threading.Thread(target=run)
+        t1.start()
+
+    time.sleep(10)
+    global stop_threads
+    stop_threads = True
+    for thread in threads:
+        thread.join()
+    print('threads were killed')
+
+stop_threads = False
+app = Flask(__name__)
+
+@app.route('/',methods=['GET'])
+def hello_world():
+    return "Hello World!", 200
+    #return "<marquee><h1>Hello World!<h1/><marquee/>"
+
+@app.route('/intense',methods=['GET'])
+def intense():
+    intense_cpu()
+    return ""
+
+if __name__ == '__main__':
+    app.run(port=5000, host='0.0.0.0', debug=True)
+
---
+++ unit_test.py
@@ -0,0 +1,22 @@
+import unittest
+import main
+
+class TestHello(unittest.TestCase):
+
+    def setUp(self):
+        main.app.testing = True
+        self.app = main.app.test_client()
+
+    def test_hello(self):
+        rv = self.app.get('/')
+        self.assertEqual(rv.status, '200 OK')
+        self.assertEqual(rv.data, b'Hello World!')
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+'''import requests
+
+res = requests.get("http://localhost:5000")
+assert "Hello World" in res.content.decode("utf-8")'''
